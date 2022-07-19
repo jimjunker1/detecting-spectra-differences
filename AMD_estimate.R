528e5d8319c9f09d8ba9eca82ae0c592188aae47
Jpomz <jfpomeranz@gmail.com>
2022-07-19 12:32:03 -0600

    big stage to save everything after resetting commit history

--- AMD_estimate.R
+++ AMD_estimate.R
@@ -10,6 +10,7 @@ amd_dat <- read_csv("site_dw_pca.csv")
 
 dw_range = range(amd_dat$dw, na.rm = TRUE)
 
+# estimate spectra paramaters across AMD gradient using three methods
 (amd_result <- amd_dat %>%
   # mutate(date = as.Date(collectDate)) %>%
   group_by(site, pca1) %>%
@@ -25,6 +26,7 @@ dw_range = range(amd_dat$dw, na.rm = TRUE)
   select(-data) %>%
   unnest(cols = method_compare))
 
+# plot parameter estimates (slope or exponent) across AMD gradient
 ggplot(amd_result,
        aes(x = pca1,
            y = estimate,
@@ -36,9 +38,11 @@ ggplot(amd_result,
   labs(title = "Change in exponent across an AMD gradient", 
        x = "AMD gradient (PCA)",
        y = "Slope/exponent estimate")
+# save the plot
 ggsave("figures/AMD_plot.png")
 
-amd_result %>%
+# estimate the beta_1_ relationship across the gradient
+amd_relationship <- amd_result %>%
   group_by(name) %>%
   nest() %>%
   mutate(lm_mod = 
@@ -47,19 +51,16 @@ amd_result %>%
   mutate(tidied = map(lm_mod, broom::tidy)) %>%
   unnest(tidied) %>%
   filter(term == "pca1") %>%
-  select(-data, -lm_mod) %>%
+  select(-data, -lm_mod) 
+
+# absolute variation across gradient
+amd_relationship %>%
+  mutate(abs_change = estimate * 
+           (max(amd_dat$pca1) - min(amd_dat$pca1))) %>%
   write_csv("results/AMD_estimates.csv")
 
-amd_result %>%
-  group_by(name) %>%
-  nest() %>%
-  mutate(lm_mod = 
-           map(data,
-               ~lm(estimate ~ pca1, data = .x))) %>%
-  mutate(tidied = map(lm_mod, broom::tidy)) %>%
-  unnest(tidied) %>%
-  filter(term == "pca1") %>%
-  select(-data, -lm_mod) %>%
+# plot the mean \pm STD.E of the estimated relationship across the gradient based on method used. 
+amd_relationship %>%
   ggplot(aes(y = estimate, 
              ymin = estimate - std.error, 
              ymax = estimate + std.error,
@@ -68,5 +69,30 @@ amd_result %>%
   geom_pointrange()+
   theme_bw() +
   labs(y = "Relationship estimate",
-       title = "Change in CSS parameter across AMD gradient")
+       title = "Mean +/- Std. Error Beta across AMD gradient")
+
 ggsave("figures/amd_relationship.png")
+
+
+
+
+
+# estimate by sample ------------------------------------------------------
+# don't think I'm doing this anymore...
+# 
+# # need to re-export data from AMD project and include "smaple" number
+# 
+# (amd_result <- amd_dat %>%
+#    # mutate(date = as.Date(collectDate)) %>%
+#    group_by(site, pca1) %>%
+#    #filter(siteID == "CARI") %>%
+#    # create list-column
+#    nest() %>%
+#    mutate(method_compare =
+#             map(data,
+#                 compare_slopes,
+#                 rsp_var = "dw",
+#                 dw_range = dw_range)) %>%
+#    ungroup() %>%
+#    select(-data) %>%
+#    unnest(cols = method_compare))
--- NEON_estimate.R
+++ NEON_estimate.R
@@ -6,9 +6,13 @@ source("custom_functions.R")
 
 # compare estimates with empirical data
 # data is from Pomeranz et al. 2018 Freshwater Biology
-neon_dat <- read_csv("neon_data.csv") 
+neon_dat <- read_csv("neon_data.csv")
 
 dw_range = range(neon_dat$dw, na.rm = TRUE)
+
+# not using the MLEbin method, as in Pomeranz et al. 2022
+# just using the "normal" MLE method to compare
+# NEON "Bins" individuals into 1 mm widths, so doesn't drastically alter body size estimates
 
 (neon_result <- neon_dat %>%
     # mutate(date = as.Date(collectDate)) %>%
@@ -34,10 +38,11 @@ ggplot(neon_result,
   labs(title = "Change in exponent across NEON", 
        x = "Mean annual air temp",
        y = "Slope/exponent estimate")
+
 ggsave("figures/NEON_plot.png")
 
 
-neon_result %>%
+neon_relationship <- neon_result %>%
   group_by(name) %>%
   nest() %>%
   mutate(lm_mod = 
@@ -46,19 +51,14 @@ neon_result %>%
   mutate(tidied = map(lm_mod, broom::tidy)) %>%
   unnest(tidied) %>%
   filter(term == "mat.c") %>%
-  select(-data, -lm_mod)%>%
+  select(-data, -lm_mod)
+
+neon_relationship %>%
+  mutate(abs_change = estimate * 
+           (max(neon_dat$mat.c) - min(neon_dat$mat.c))) %>%
   write_csv("results/NEON_estimates.csv")
 
-neon_result %>%
-  group_by(name) %>%
-  nest() %>%
-  mutate(lm_mod = 
-           map(data,
-               ~lm(estimate ~ mat.c, data = .x))) %>%
-  mutate(tidied = map(lm_mod, broom::tidy)) %>%
-  unnest(tidied) %>%
-  filter(term == "mat.c") %>%
-  select(-data, -lm_mod) %>%
+neon_relationship %>%
   ggplot(aes(y = estimate, 
              ymin = estimate - std.error, 
              ymax = estimate + std.error,
@@ -67,5 +67,102 @@ neon_result %>%
   geom_pointrange()+
   theme_bw() +
   labs(y = "Relationship estimate",
-       title = "Change in CSS parameter across Neon")
+       title = "Mean +/- Std. Error Beta across Neon")
+
 ggsave("figures/neon_relationship.png")
+
+
+
+# estimate by sample ------------------------------------------------------
+
+# not sure where to go with this want to compare what the estimates are for each sample (i.e. surber sample), and see how that compares to the avergae across samples, and the estimate when combining all of the data for one estimate. 
+
+# need to re-export NEON data from project and include sample number
+# estimate CSS parameter for each sample
+# compare "total" estimate with individual sample estimates and mean_sample estimates
+
+(neon_sample_result <- neon_dat %>%
+   group_by(mat.c, ID, sample) %>%
+   mutate(n = n()) %>%
+   filter(n >= 1000) %>% # filter out samples that have < 1000 individuals
+   nest() %>%
+   mutate(method_compare =
+            map(data,
+                compare_slopes,
+                rsp_var = "dw",
+                dw_range = dw_range)) %>%
+   ungroup() %>%
+   select(-data) %>%
+   unnest(cols = method_compare))
+
+ggplot(neon_sample_result,
+       aes(x = mat.c,
+           y = estimate,
+           color = name))+
+  geom_point(position = position_jitter(widt = 0.2),
+             alpha = 0.5) +
+  stat_smooth(method = "lm",
+              se = FALSE)+
+  theme_bw() +
+  labs(title = "Change in exponent across NEON", 
+       x = "Mean annual air temp",
+       y = "Slope/exponent estimate")
+
+neon_sample_result %>%
+  group_by(name) %>%
+  nest() %>%
+  mutate(lm_mod = 
+           map(data,
+               ~lm(estimate ~ mat.c, data = .x))) %>%
+  mutate(tidied = map(lm_mod, broom::tidy)) %>%
+  unnest(tidied) %>%
+  filter(term == "mat.c") %>%
+  select(-data, -lm_mod)
+
+ggplot(neon_sample_result,
+       aes(x = mat.c,
+           y = estimate))+
+  geom_point(position = position_jitter(widt = 0.2),
+             alpha = 0.5) +
+  stat_summary(aes(group = ID), fun.data = "mean_se", color = "red", size = 0.5) +
+  stat_smooth(method = "lm",
+              se = FALSE)+
+  theme_bw() +
+  geom_point(inherit.aes = FALSE,
+             data = neon_result, 
+             aes(x = mat.c, 
+                 y = estimate),
+             color = "blue",
+             size = 1) +
+  facet_wrap(~name, 
+             ncol = 1)
+
+neon_sample_result %>%
+  filter(ID == 1) %>%
+ggplot(
+       aes(x = mat.c,
+           y = estimate))+
+  geom_point(position = position_jitter(),
+             alpha = 0.5) +
+  stat_summary(aes(group = ID),
+               fun.data = "mean_se",
+               color = "red",
+               size = 0.5) +
+  stat_smooth(method = "lm",
+              se = FALSE)+
+  theme_bw() +
+  geom_pointrange(
+    inherit.aes = FALSE,
+    data = neon_result %>%
+      filter(ID == 1),
+    aes(x = mat.c,
+        y = estimate,
+        ymin = minCI, 
+        ymax = maxCI),
+    color = "dodgerblue") +
+  facet_wrap(~name, 
+             ncol = 1) +
+  labs(title = "NEON Sample Estimates", 
+       subtitle = "Black = sample, red = mean sample, blue = combined",
+       y = "Slope/exponent estimate") +
+  NULL
